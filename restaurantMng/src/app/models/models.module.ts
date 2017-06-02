055483e0f22ec9aeed1a17692eda1a9fbf83828d
Melanie Liang <melly.strearshy@gmail.com>
2017-06-01 19:46:38 -0600

    new dish ts

---
+++ restaurantMng/src/app/models/models.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export class ModelsModule {
+  private name: string;
+  private price: number;
+  private description: string;
+  private categotyName: string;
+
+  constructor(name: string, price: number, description: string, categotyName: string) {
+    this.setName(name);
+    this.setPrice(price);
+    this.setDescription(description);
+    this.setCategotyName(categotyName);
+  }
+  getName() { return this.name; }
+  getPrice() { return this.price; }
+  getDescription() { return this.description; }
+  getCategotyName() { return this.categotyName; }
+}
--- restaurantMng/src/app/new-dish-form/new-dish-form.component.ts
+++ restaurantMng/src/app/new-dish-form/new-dish-form.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { NgModule } from models
 @Component({
   selector: 'app-new-dish-form',
   templateUrl: './new-dish-form.component.html',
   styleUrls: ['./new-dish-form.component.css']
 })
 export class NewDishFormComponent implements OnInit {
+  name = "";
+  price = "";
+  desccription = "";
+  categotyName = "";
 
   constructor() { }
 
